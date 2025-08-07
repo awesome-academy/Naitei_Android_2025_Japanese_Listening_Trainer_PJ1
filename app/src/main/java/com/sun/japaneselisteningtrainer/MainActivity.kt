20cfe4dd8a12293088e97d0bc2ccc4af551506d4
Nguyen Duc Manh <167436213+manhnd52@users.noreply.github.com>
2025-08-07 09:21:36 +0700

    init project (#5)

---
+++ app/src/main/java/com/sun/japaneselisteningtrainer/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sun.japaneselisteningtrainer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sun.japaneselisteningtrainer.ui.theme.JapaneseListeningTrainerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JapaneseListeningTrainerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JapaneseListeningTrainerTheme {
+        Greeting("Android")
+    }
+}
