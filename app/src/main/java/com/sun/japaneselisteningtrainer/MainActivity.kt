88b694798a7e5f5c1de70a02409b9ffbf08acb0a
Nguyen Duc Manh <167436213+manhnd52@users.noreply.github.com>
2025-08-13 10:09:55 +0700

    apply-theme

--- app/src/main/java/com/sun/japaneselisteningtrainer/MainActivity.kt
+++ app/src/main/java/com/sun/japaneselisteningtrainer/MainActivity.kt
@@ -32,18 +32,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun TrainerAppPreview() {
     JapaneseListeningTrainerTheme {
-        Greeting("Android")
+        TrainerApp()
     }
 }
